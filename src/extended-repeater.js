b809b8b28732233bf9a8577917582a03033c2589
Denis <dennimail@gmail.com>
2022-04-30 18:48:47 +0300

    feat: extended repeater is done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  this.repeatTimes = options.repeatTimes ? parseInt(options.repeatTimes) : 1
+  this.separator = options.separator ? options.separator.toString() : ""
+  this.addition = options.addition ? options.addition.toString() : ""
+  this.additionRepeatTimes = options.additionRepeatTimes ? parseInt(options.additionRepeatTimes) : 1
+  this.additionSeparator = options.additionSeparator ? options.additionSeparator.toString() : ""
+  this.str = str.toString()
+  let addittion = this.addition
+  for (let j=0; j< this.additionRepeatTimes-1;j++){
+    addittion=addittion+this.additionSeparator+this.addition
+  }
+  let Str = this.str+addittion
+  let outerStr = Str
+  for (let i=0; i< this.repeatTimes-1;i++){
+    outerStr =  outerStr + this.separator + Str
+  }
+  console.log(outerStr)
 }
+
+repeater('STRING', { 
+   })
 
 module.exports = {
   repeater
