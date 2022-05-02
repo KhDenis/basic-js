5a315253177faf5c545659ccce225a0a0ccdfc9a
Denis <dennimail@gmail.com>
2022-05-02 10:22:34 +0300

    feat: transform array almost done

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,14 +9,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * @example
  * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
+ * --discard-next excludes the next element of the array from the transformed array.
+ --discard-prev excludes the previous element of the array from the transformed array.
+ --double-next duplicates the next element of the array in the transformed array.
+ --double-prev duplicates the previous element of the array in the transformed array.
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])) //=> [1, 2, 3, 4, 5]
+
+function transform(arr) {
+  let arr2
+  if(Array.isArray(arr)){
+    arr2 = arr.map(e=> e=e)
+  }else{
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  let validation = ["--discard-next","--discard-prev","--double-next","--double-prev"]
+  
+  function mkNewArr(newArr){
+
+    console.log(newArr)
+    // return
+    newArr.forEach((e,id) => {
+      if((e == "--discard-next")){
+        if(id<newArr.length-1){
+          newArr.splice(id,2) 
+          mkNewArr(newArr) 
+        }else {newArr.splice(id,1); mkNewArr(newArr)}
+      }
+      else if(e == "--discard-prev"){
+        if(id>0){
+         newArr.splice(id-1,2)
+         mkNewArr(newArr)
+        }else {newArr.splice(id,1); mkNewArr(newArr)}
+      }
+      else if(e == "--double-next"){
+        newArr[id] = newArr[id+1]
+      }
+      else if(e == "--double-prev"){
+        newArr[id] = newArr[id-1]
+      }else if(typeof(e)!="number"){
+        newArr = newArr.splice(0)
+        return newArr
+      }
+    })
+    return newArr
+  }
+  return  mkNewArr(arr2)
 }
+
 
 module.exports = {
   transform
